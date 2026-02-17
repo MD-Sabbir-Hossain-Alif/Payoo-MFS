b102b074e8c2e6bb3a6c79e1208daf11d8afd4ca
MD-Sabbir-Hossain-Alif <sabbir.web.dp@gmail.com>
2026-02-18 03:31:44 +0600

    homme + cashout + cashout.js

--- script/login.js
+++ script/login.js
@@ -2,11 +2,30 @@
 
 // TODO: create login button event:
 document.getElementById("login-btn")
-    .addEventListener("click", function() {
+    .addEventListener("click", function(event) {
+
+        event.preventDefault();
 
         // step-1: get the mobile number input
+        const numberInput = document.getElementById("input-number");
+        const contactNumber = numberInput.value;
+
         // step-2: get the pin input
+        const pinInput = document.getElementById("input-pin");
+        const pin = pinInput.value;
+
         // step-3: metch mobile & pin 
-        // step-3.1 if true::alert::>homepage
-        // step-3.2 if false::alert::>return
+        if (contactNumber === "01300000000" && pin === "9898") {
+            // step-3.1 if true::alert::>homepage
+            alert("login Success");
+            window.location.assign("./home.html");
+        } else {
+            // step-3.2 if false::alert::>return
+            alert("login Failed");
+            setTimeout(() => {
+                window.location.reload();
+            }, 100);
+        }
+        
+        
     });
