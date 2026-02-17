583b344727938fb49530d1d5135714ded27032f6
MD-Sabbir-Hossain-Alif <sabbir.web.dp@gmail.com>
2026-02-18 00:25:25 +0600

    add form &  create login.js

---
+++ script/login.js
@@ -0,0 +1,12 @@
+/* -------------------------- Functional Login Form (using vanilla js) ------------------------- */
+
+// TODO: create login button event:
+document.getElementById("login-btn")
+    .addEventListener("click", function() {
+
+        // step-1: get the mobile number input
+        // step-2: get the pin input
+        // step-3: metch mobile & pin 
+        // step-3.1 if true::alert::>homepage
+        // step-3.2 if false::alert::>return
+    });
